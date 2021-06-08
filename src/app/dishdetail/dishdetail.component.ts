42c95db5199d05c4dfb8b9a153e28ce7f3464bcd
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-06-08 22:09:21 +0530

    RxJs Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,7 +20,7 @@ export class DishdetailComponent implements OnInit {
   ngOnInit(): void {
     const id1 = +this.route.snapshot.params['id'];
     const id = String(id1);
-    this.dishservice.getDish(id).then(dish => this.dish =  dish);
+    this.dishservice.getDish(id).subscribe(dish => this.dish =  dish);
   }
 
   goBack(): void {
